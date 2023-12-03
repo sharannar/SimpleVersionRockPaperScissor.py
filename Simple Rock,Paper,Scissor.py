0fb5332ead3c37daee927b11f67cc6e1990fbfd5
Sharanna Rushing <83149036+sharannar@users.noreply.github.com>
2023-12-02 20:31:16 -0500

    Create Simple Rock,Paper,Scissor.py

---
+++ Simple Rock,Paper,Scissor.py
@@ -0,0 +1,12 @@
+import random
+
+def main():
+    roshambo = ["Rock","Paper", "Scissors"]
+    computer_choice = random.choice (roshambo)
+    num = input("1 for Rock, 2 for Paper, 3 for scissors:")
+    num = int(num)-1
+    user_choice = roshambo[num]
+    print ("Computer:",computer_choice)
+    print("User:",user_choice)
+
+main()
